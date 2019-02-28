bc0fc17167f277dbd6385ea98cd1c679aa652596
Juan Latorre <juanlatorre@protonmail.com>
2019-02-28 11:12:17 -0300

    .

--- config.sample.php
+++ config.sample.php
@@ -1,7 +1,7 @@
 <?php
-$dbhost="localhost";
-$dbuser="root";
-$dbpass="root";
+$dbhost="YOUR_DB_HOST";
+$dbuser="YOUR_DB_USER";
+$dbpass="YOUR_DB_PASS";
 $dbname="encuesta";
 $db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ("Error conectando a la base de datos.");
 mysqli_query($db, "SET NAMES 'utf8'");
